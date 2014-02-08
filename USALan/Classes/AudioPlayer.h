e3af4b1ad68fdc4d55598d9aebcbc891f30d4f4f
LM <sh00008@sohu.com>
2014-02-08 16:10:36 +0800

    添加播放的一些文件。

---
+++ USALan/Classes/AudioPlayer.h
@@ -0,0 +1,13 @@
+//
+//  AudioPlayer.h
+//  USALan
+//
+//  Created by JiaLi on 14-2-8.
+//  Copyright (c) 2014年 JiaLi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AudioPlayer : NSObject
+
+@end
---
+++ USALan/Classes/AudioPlayer.m
@@ -0,0 +1,13 @@
+//
+//  AudioPlayer.m
+//  USALan
+//
+//  Created by JiaLi on 14-2-8.
+//  Copyright (c) 2014年 JiaLi. All rights reserved.
+//
+
+#import "AudioPlayer.h"
+
+@implementation AudioPlayer
+
+@end
