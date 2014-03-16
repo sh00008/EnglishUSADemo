e1acaa5c40c631843e2cfc8f7fd54ee0998e71b4
sh00008 <sh00008@sohu.com>
2014-03-17 01:15:33 +0800

    添加缩略图的一些代码。

---
+++ USALan/Classes/ThumbViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThumbViewController.h
+//  USALan
+//
+//  Created by JiaLi on 14-3-17.
+//  Copyright (c) 2014年 JiaLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThumbViewController : UICollectionViewController
+
+@end
